e6dbae655ba6a971239c092a88c84e6236ce2638
Killian LUTZ <killian.lutz@math.unistra.fr>
2023-11-12 18:05:21 +0100

    main animations defined; ready to be saved once tests are made

--- scripts/run_animation.jl
+++ scripts/run_animation.jl
@@ -10,51 +10,51 @@ set_theme!(theme_dark())
 
 #####
 # sequence parameters
-nframe = 1500
+nframe = 250
 npoints = 40
-xygrid_lims = (-5.0, 5.0, -5.0, 5.0)
+xygrid_lims = (-1.0, 2.0, -1.0, 2.0)#(-5.0, 5.0, -5.0, 5.0)
 params = (; nframe, npoints, xygrid_lims)
 
 " gradient step "
-fig, iter = setup_gradient(params...; step_size = 0.005, objective_type = "multimodal")
+fig, step_forward = setup_gradient(params...; step_size = 0.005, objective_type = "multimodal")
 # rosenbrock : 0.01, 0.005, 0.004, 0.001 ; 1500 frames ; (0.5, 1.2, 0.5, 1.2) or (-1.5, 1.5, -1.5, 1.5)
 " gradient step in 3D "
-fig, iter = setup_gradient3d(params...; step_size = 0.05, objective_type = "gaussian")
+fig, step_forward = setup_gradient3d(params...; step_size = 0.05, objective_type = "gaussian")
 
 " optimal step gradient "
-fig, iter = setup_linesearch_gradient(params...; step_size = 0.1, objective_type = "multimodal")
+fig, step_forward = setup_linesearch_gradient(params...; step_size = 0.1, objective_type = "multimodal")
 
 " projected gradient "
-fig, iter = setup_projgradient(params...; step_size = 0.01, objective_type = "1modeat0", constraint_type = "box")
+fig, step_forward = setup_projgradient(params...; step_size = 0.01, objective_type = "1modeat0", constraint_type = "box")
 
 " uzawa/arrow-hurwicz "
 A = [1.0 0.05; 0.05 1.0] # positive definite ?
 b = A*[1.5, 1.2]
-fig, iter = setup_primaldual3d(params...; step_size = 0.05, algorithm = "arrowhurwicz", A, b)
-# step_size 0.05 for uzawa (-1.0, 2.0, -1.0, 2.0); 0.05 for arrowhurwicz and limits (-1.0, 2.0, -1.0, 2.0)
+fig, step_forward = setup_primaldual3d(params...; step_size = 0.05, algorithm = "arrowhurwicz", A, b)
+# step size 0.05 for uzawa/arrowhurwicz and limits (-1.0, 2.0, -1.0, 2.0)
 
 " augmented lagrangian "
 A = [1.0 0.05; 0.05 1.0] # positive definite ?
 b = A*[1.5, 1.2]
 B = [1.0 0; 0 1.0] # try with anisotropic axes
-fig, iter = setup_augmented_lagrangian3d(params...; step_size = 1.0, A, b, B, grad_step = 0.001, grad_iter = 200)
+fig, step_forward = setup_augmented_lagrangian3d(params...; step_size = 1.0, A, b, B, grad_step = 0.001, grad_iter = 200)
 
 " penalized gradient 2D / 3D "
-fig, iter = setup_penalized_gradient(params...; step_size = 0.01, objective_type = "gaussian", ϵ = 1e-0)
+fig, step_forward = setup_penalized_gradient(params...; step_size = 0.01, objective_type = "gaussian", ϵ = 1e-0)
 
-fig, iter = setup_penalized_gradient3d(params...; step_size = 0.0001, objective_type = "gaussian", ϵ = 1e-1)
+fig, step_forward = setup_penalized_gradient3d(params...; step_size = 0.0001, objective_type = "gaussian", ϵ = 1e-1)
 
 display(fig)
 for i in 1:nframe
     i == 1 ? sleep(2.0) : sleep(0.01)
-    iter(i)
+    step_forward(i)
 end
 
 # # with periodic cameras moves 
-# fig, iter = setup_gradient3d(params...; step_size = 0.05, objective_type = "gaussian")
+# fig, step_forward = setup_gradient3d(params...; step_size = 0.05, objective_type = "gaussian")
 # ax = fig.content[1]
 # for i in 1:nframe
-#     iter()
+#     step_forward(i)
 #     ax.azimuth[] = 1.7π + 3.0 * sin(2π * i / 1000)
 #     ax.elevation[] = 0.15 + 1.0 * sin(2π * i / 800)
 #     sleep(0.01)
--- src/make_animations.jl
+++ src/make_animations.jl
@@ -35,7 +35,7 @@ function save_descent_(animations::Dict)
         elseif !ismissing(anim[:dynamic])
             finishes_with = "_dynamic"
         end
-        "$(anim[:method])_$(anim[:objective])_$(anim[:dim])_step$(replace_float2string(anim[:step]))$(finishes_with).mp4"
+        "$(anim[:method])/$(anim[:objective])_$(anim[:dim])_step$(replace_float2string(anim[:step]))$(finishes_with).mp4"
     end
 
     foreach(animations) do x
@@ -88,99 +88,238 @@ function save_descent()
 end
 
 ################ linesearch ################
+function save_linesearch_(animations::Dict)
+    # method specific
+    setups = Dict(
+        dim => ((par, anim) -> setup(par...; step_size=anim[:step], objective_type=anim[:objective])) 
+        for (dim, setup) in zip(("2D", "3D"), (setup_linesearch_gradient, setup_linesearch_gradient))
+    )
+
+    to_filename(anim) = "$(anim[:method])/$(anim[:objective])_$(anim[:dim])_step$(replace_float2string(anim[:step])).mp4"
+
+    foreach(animations) do x
+        setup_and_record(x, setups, to_filename)
+    end
+end
+
 function save_linesearch()
-    
+    animations = Vector{Any}[]
+    base_animation = Dict(
+        :method => "linesearch",
+        :objective => "gaussian",
+        :constraint => missing,
+        :dim => "2D",
+        :nframe => 20,
+        :xylims => (-5.0, 5.0, -5.0, 5.0),
+        :step => 0.1,
+    )
+
+    for objective in ("gaussian", "multimodal")
+        animation = deepcopy(base_animation)
+        animation[:objective] = objective
+
+        push!(animations, animation)
+    end
+
+    save_linesearch_(animations)
 end
 
 ################ projected ################
+function save_projected_(animations::Dict)
+    # method specific
+    setups = Dict(
+        dim => ((par, anim) -> setup(par...; step_size=anim[:step], objective_type=anim[:objective], constraint_type=anim[:constraint])) 
+        for (dim, setup) in zip(("2D", "3D"), (setup_projgradient, setup_projgradient))
+    )
+
+    to_filename(anim) = "$(anim[:method])/$(anim[:objective])_$(anim[:dim])_step$(replace_float2string(anim[:step]))_$(anim[:constraint]).mp4"
+
+    foreach(animations) do x
+        setup_and_record(x, setups, to_filename)
+    end
+end
+
 function save_projected()
-    
+    animations = Vector{Any}[]
+    base_animation = Dict(
+        :method => "projected",
+        :objective => "gaussian",
+        :constraint => "ball",
+        :dim => "2D",
+        :nframe => 800,
+        :xylims => (-5.0, 5.0, -5.0, 5.0),
+        :step => 0.0005,
+    )
+
+    objective_to_nframe = Dict("gaussian" => 800, "multimodal" => 1000, "1modeat0" => 300)
+    for objective in ("gaussian", "multimodal", "1modeat0"), constraint in ("ball", "box")
+        animation = deepcopy(base_animation)
+        animation[:objective] = objective
+        animation[:constraint] = constraint
+        animation[:nframe] = objective_to_nframe[objective]
+
+        push!(animations, animation)
+    end
+
+    save_projected_(animations)
 end
 
 ################ penalized ################
+function save_penalized_(animations::Dict)
+    # method specific
+    setups = Dict(
+        dim => ((par, anim) -> setup(par...; step_size=anim[:step], objective_type=anim[:objective], ϵ=anim[:ϵ])) 
+        for (dim, setup) in zip(("2D", "3D"), (setup_penalized_gradient, setup_penalized_gradient3d))
+    )
+
+    to_filename(anim) = "$(anim[:method])/$(anim[:objective])_$(anim[:dim])_step$(replace_float2string(anim[:step]))_ϵ$(anim[:ϵ]).mp4"
+
+    foreach(animations) do x
+        setup_and_record(x, setups, to_filename)
+    end
+end
+
 function save_penalized()
-    
+    animations = Vector{Any}[]
+    base_animation = Dict(
+        :method => "penalized",
+        :objective => "gaussian",
+        :constraint => "ball",
+        :dim => "2D",
+        :nframe => 1500,
+        :xylims => (-5.0, 5.0, -5.0, 5.0),
+        :step => 0.01,
+        :ϵ => 1.0
+    )
+
+    for objective in ("gaussian", "multimodal", "1modeat0"), step in (1e-2, 1e-4), ϵ in (1.0, 0.1)
+        animation = deepcopy(base_animation)
+        animation[:objective] = objective
+        animation[:step] = step
+        animation[:ϵ] = ϵ
+
+        push!(animations, animation)
+    end
+
+    save_penalized_(animations)
 end
 
 ################ uzawa ################
+function save_uzawa_(animations::Dict)
+    # method specific
+    setups = Dict(
+        dim => ((par, anim) -> setup(par...; step_size=anim[:step], algorithm=anim[:algo], anim[:A], anim[:b])) 
+        for (dim, setup) in zip(("2D", "3D"), (setup_primaldual3d, setup_primaldual3d))
+    )
+
+    to_filename(anim) = "$(anim[:method])/quadratic_$(anim[:dim])_step$(replace_float2string(anim[:step])).mp4"
+
+    foreach(animations) do x
+        setup_and_record(x, setups, to_filename)
+    end
+end
+
 function save_uzawa()
-    
+    A = [1.0 0.05; 0.05 1.0]
+    animations = Vector{Any}[]
+    base_animation = Dict(
+        :method => "uzawa",
+        :dim => "3D",
+        :nframe => 250,
+        :xylims => (-1.0, 2.0, -1.0, 2.0),
+        :step => 0.05,
+        :algo => "uzawa"
+        :A => A,
+        :b => A*[1.5, 1.2]
+    )
+
+    animation = deepcopy(base_animation)
+    push!(animations, animation)
+
+    save_uzawa_(animations)
 end
 
 ################ step_arrowhurwicz ################
+function save_arrowhurwicz_(animations::Dict)
+    # method specific
+    setups = Dict(
+        dim => ((par, anim) -> setup(par...; step_size=anim[:step], algorithm=anim[:algo], anim[:A], anim[:b])) 
+        for (dim, setup) in zip(("2D", "3D"), (setup_primaldual3d, setup_primaldual3d))
+    )
+
+    to_filename(anim) = "$(anim[:method])/quadratic_$(anim[:dim])_step$(replace_float2string(anim[:step])).mp4"
+
+    foreach(animations) do x
+        setup_and_record(x, setups, to_filename)
+    end
+end
+
 function save_arrowhurwicz()
-    
+    A = [1.0 0.05; 0.05 1.0]
+    animations = Vector{Any}[]
+    base_animation = Dict(
+        :method => "arrowhurwicz",
+        :dim => "3D",
+        :nframe => 150,
+        :xylims => (-1.0, 2.0, -1.0, 2.0),
+        :step => 0.05,
+        :algo => "arrowhurwicz"
+        :A => A,
+        :b => A*[1.5, 1.2]
+    )
+
+    animation = deepcopy(base_animation)
+    push!(animations, animation)
+
+    save_arrowhurwicz_(animations)
 end
 
 ################ augmented lagrangian ################
-function save_augmented_lagrangian()
-    
+function save_augmented_lagrangian_(animations::Dict)
+    # method specific
+    setups = Dict(
+        dim => ((par, anim) -> setup(par...; step_size=anim[:step], A=anim[:A], b=anim[:b], B=anim[:B], grad_step=anim[:grad_step], grad_iter=anim[:grad_iter])) 
+        for (dim, setup) in zip(("2D", "3D"), (setup_augmented_lagrangian3d, setup_augmented_lagrangian3d))
+    )
+
+    to_filename(anim) = "$(anim[:method])/quadratic_$(anim[:dim])_step$(replace_float2string(anim[:step]))_$(anim[:constraint]).mp4"
+
+    foreach(animations) do x
+        setup_and_record(x, setups, to_filename)
+    end
 end
 
+function save_augmented_lagrangian()
+    A = [1.0 0.05; 0.05 1.0]
+    B = ([1.0 0; 0 1.0], [1.0 0; 0 4.0])
+    animations = Vector{Any}[]
+    base_animation = Dict(
+        :method => "augmented_lagrangian",
+        :constraint => "ball"
+        :dim => "3D",
+        :nframe => 50,
+        :xylims => (-1.0, 2.0, -1.0, 2.0),
+        :step => 1.0,
+        :A => A,
+        :b => A*[1.5, 1.2],
+        :B => missing,
+        :grad_step => 0.001,
+        :grad_iter => 200
+    )
+
+    constraint_to_B = Dict("ball" => first(B), "ellipsoid" => last(B))
+    for constraint in ("ball", "ellipsoid")
+        animation = deepcopy(base_animation)
+        animation[:constraint] = constraint
+        animation[:B] = constraint_to_B[constraint]
+
+        push!(animations, animation)
+    end
+
+    save_augmented_lagrangian_(animations)
+end
 
 ################ else 
 function replace_float2string(x)
     replace(string(x), "." => "_")
 end
-
-
-## record animation
-# fig, iter = setup_projgradient(params...; step_size = 0.0005, objective_type = "multimodal", constraint_type = "ball")
-# record(fig, "descent_projected_multimodal_ball.mp4", 1:nframe; framerate = 24) do i
-#     if i <= 24
-#         display(fig)
-#     else
-#         iter()
-#     end
-# end
-
-# fig, iter = setup_gradient(params...; step_size = 0.005, objective_type = "rosenbrock")
-# display(fig)
-# record(fig, "descent_rosenbrock2D_step0_005_zoom.mp4", 1:nframe; framerate = 24) do _
-#     iter()
-# end
-
-# fig, iter = with_theme(theme_dark()) do 
-#     setup_gradient3d(params...; step_size = 0.05, objective_type = "gaussian")
-# end
-# ax = fig.content[1]
-# display(fig)
-# record(fig, "descent_gaussian_dynamic.mp4", 1:nframe; framerate = 24) do frame
-#     ax.azimuth[] = 1.7π + 3.0 * sin(2π * frame / 1000)
-#     ax.elevation[] = 0.15 + 1.0 * sin(2π * frame / 800)
-#     iter()
-# end
-
-# fig, iter = with_theme(theme_dark()) do 
-#     setup_gradient3d(params...; step_size = 0.1, objective_type = "gaussian")
-# end
-# display(fig)
-# record(fig, "gaussian.mp4", 1:nframe; framerate = 24) do _
-#     iter()
-# end
-
-# fig, iter = setup_gradient(params...; step_size = 0.0005, objective_type = "multimodal")
-# display(fig)
-# record(fig, "descent_multimodal2D.mp4", 1:nframe; framerate = 24) do _
-#     iter()
-# end
-
-# fig, iter = setup_linesearch_gradient(params...; step_size = 0.1, objective_type = "multimodal")
-# display(fig)
-# record(fig, "linesearch_descent_multimodal2D.mp4", 1:nframe; framerate = 1) do i
-#     i <= 1 ? display(fig) : iter()
-# end
-
-# fig, iter = setup_linesearch_gradient(params...; step_size = 0.1, objective_type = "gaussian")
-# display(fig)
-# record(fig, "linesearch_descent_gaussian2D_maxstep0.1.mp4", 1:nframe; framerate = 1) do i
-#     i <= 1 ? display(fig) : iter()
-# end
-
-# fig, iter = with_theme(theme_dark()) do 
-#     setup_gradient3d(params...; step_size = 0.1, objective_type = "gaussian")
-# end
-# display(fig)
-# record(fig, "descent_gaussian.mp4", 1:nframe; framerate = 24) do _
-#     iter()
-# end
